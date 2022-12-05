4dc404465c872a53ee4a496603305e616836212f
Ahmad Naji <65174502+AhmadKNaji@users.noreply.github.com>
2022-12-04 20:50:18 -0500

    script to create csvs

---
+++ processing/creating_csvs.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.path.insert(1, '../processing')
+
+from twitter import Tweets
+
+from data_clean import clean_tweet
+
+import pandas as pd
+
+def update_tweets(nb):
+
+    tweets = Tweets('elonmusk')
+
+    data = tweets.get_tweets(nb)
+
+    datac = data.copy()
+
+    pd.DataFrame.to_csv(datac, 'assets/csv files/raw_tweets.csv')
+
+    datac['Tweet'] = datac['Tweet'].apply(lambda tweet: clean_tweet(tweet))
+
+    pd.DataFrame.to_csv(datac, 'assets/csv files/unified_tweets.csv')
+
+    data_retweets = datac[['Tweet', 'Time', 'nb_retweets']]
+
+    data_favorites = datac[['Tweet', 'Time', 'nb_favorites']]
+
+    pd.DataFrame.to_csv(data_retweets, 'assets/csv files/data/retweets.csv')
+
+    pd.DataFrame.to_csv(data_favorites, 'assets/csv files/data/favorites.csv')
+
+update_tweets(1000)
